e9d4342bf3b6bfe241f7197e4473ecaffdb0907d
RehamHamed33 <remohamed2003@gmail.com>
2024-12-13 22:37:08 +0200

    intial commit

--- LBMS/LBMS/Program.fs
+++ LBMS/LBMS/Program.fs
@@ -1,2 +1,26 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Windows.Forms
+
+type Book =
+    { Title: string
+      Author: string
+      Genre: string
+      IsBorrowed: bool
+      BorrowDate: DateTime option
+      Borrower: string }
+
+
+let mutable libraryBooks = Map.empty<string, Book>
+
+
+
+
+// Windows Forms UI
+let form = new Form(Text = "Library Management System", Width = 600, Height = 600)
+
+
+
+[<EntryPoint>]
+let main _ =
+    Application.Run(form)
+    0
